4c81a1ae58dd9acf99e661febf207aa8822b184d
peltokukka <markku.akerblom@gmail.com>
2017-12-08 08:51:32 +0200

    Fixed function scope error
    Rotation_matrix() function was not in the scope of the plot_cylinders()
    method of the QSMBCylindrical class.

--- src/classes/@QSMBCylindrical/QSMBCylindrical.m
+++ src/classes/@QSMBCylindrical/QSMBCylindrical.m
@@ -216,9 +216,133 @@ classdef QSMBCylindrical < QSMB
 
         end
        
-        % Function for plotting cylinders in model. Implementation is
-        % external.
-        h = plot_cylinders(ob,varargin)
+        function h = plot_cylinders(ob,varargin)
+        % Draw cylinders of the QSM. The following extra parameters are
+        % available for customization, passed in label-value pairs:
+        %
+        % 'Points'      Number of vertices in cylinder rings.
+        %
+        % 'Curve'       Taper curve passed to the CYLINDER command.
+        %
+        % 'Color'       Color for the cylinders.
+        %
+
+            cdata = [];
+            curve = 1;
+            points = 20;
+
+            % Process additional arguments.
+            i = 1;
+            while i <= size(varargin,2)
+
+                if ischar(varargin{i})
+
+                    switch lower(varargin{i})
+                        case 'points'
+                            points = varargin{i+1};
+                            i = i + 1;
+                        case 'curve'
+                            curve = varargin{i+1};
+                            i = i + 1;
+                        case 'color'
+                            cdata = varargin{i+1};
+                            i = i + 1;
+                        otherwise
+                            disp(['Ignoring unknown parameter: ' varargin{i}]);
+                    end
+                end
+                i = i + 1;
+            end
+
+            % Generate vertices and faces for a unit cylinder with a given number of
+            % vertices in each ring and a given taper curve.
+            [x, y, z] = cylinder(curve,points);
+
+            % Number of vertices in cylinder.
+            s = size(x);
+
+            % Number of cylinders in QSM.
+            n = ob.block_count;
+
+            % Reshape vertices.
+            x = repmat(x(:)',n,1);
+            y = repmat(y(:)',n,1);
+            z = repmat(z(:)',n,1);
+
+            % Scale to adjust correct radius and length.
+            x = bsxfun(@times,x,ob.cylinder_radius);
+            y = bsxfun(@times,y,ob.cylinder_radius);
+            z = bsxfun(@times,z,ob.cylinder_length);
+
+            u = [0; 0; 1];
+
+            % Origins of cylinders.
+            start_points = ob.cylinder_start_point;
+
+            % Directions of cylinders.
+            ax = ob.cylinder_axis;
+
+            % Find rotation angle to rotate the cylinders.
+            raxis = (bsxfun(@cross,u,ax'))';
+            angle = acos(ax(:,3)./sqrt(sum(ax.^2,2)));
+
+            % Store figure hold status.
+            set_hold_off = ~ishold;
+
+            % Variable to hold handles to graphical objects.
+            h = zeros(n,1);
+
+            % Iterate over cylinders.
+            for i = 1:n
+
+                % Set hold on after plotting first cylinder.
+                if i == 2
+                    hold on
+                end
+
+                % If the cylinder is not parallel to z-direction. Rotate vertices by
+                % previously computed angle.
+                if any(raxis(i,:))
+                    X = [x(i,:)' y(i,:)' z(i,:)'] ...
+                      * rotation_matrix(raxis(i,:)...
+                      / norm(raxis(i,:)),angle(i))';
+                %- 
+
+                % Downward pointing cylinder.
+                elseif ax(i,3) == -1
+                    X = [x(i,:)' y(i,:)' -z(i,:)'];
+                % Upward pointing cylinder.
+                else
+                    X = [x(i,:)' y(i,:)' z(i,:)'];
+                end
+
+                % Reshape to size which is required by the surf-command.
+                xi = reshape(X(:,1),s);
+                yi = reshape(X(:,2),s);
+                zi = reshape(X(:,3),s);
+
+                % Transfer computed cylinder to the starting point of the branch.
+                xi = xi + start_points(i,1);
+                yi = yi + start_points(i,2);
+                zi = zi + start_points(i,3);
+
+                % Draw the surface of the branch.
+                if ~isempty(cdata)
+                    h(i) = surf(xi,yi,zi,repmat(cdata(i,:),size(zi,1),size(zi,2)));
+                else
+                    h(i) = surf(xi,yi,zi);
+                end
+
+            end
+
+            % Set axis scaling equal.
+            axis equal;
+
+            % Restore the hold status if necessary.
+            if set_hold_off
+                hold off;
+            end
+        end
 
         function Props = get_block_properties(ob,PropNames,vargin)
         % Function to read (or compute, in some cases) properties of
